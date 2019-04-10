df5ab1acce9ae2898fd70330cc912b54bda6e263
Cabaret Voltaire <medinaisrael_17@yahoo.com>
2019-04-10 16:23:29 -0500

    database created with burgers

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ("Bacon Cheeseburger");
+INSERT INTO burgers (burger_name) VALUES ("Gut Grenade");
+INSERT INTO burgers (burger_name) VALUES ("Dirty Patty Melt");
